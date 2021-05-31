481c98d82af4d3bdf3a19d46bee0205bbf55e157
GITDLOL <72979418+GITDLOL@users.noreply.github.com>
2021-05-31 16:48:14 +0700

    It's working like it's supposed to.

---
+++ ajs.js
@@ -0,0 +1,14 @@
+// JUST BASIC THINGS
+
+var ok = "YES";
+
+console.log(ok);
+
+
+// IF STATEMENT
+if (ok == "YES") {
+    console.log("OH YES");
+}
+else if (ok == "NO") {
+    console.log("OH NO!");
+}
